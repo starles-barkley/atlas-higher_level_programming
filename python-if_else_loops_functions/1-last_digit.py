fbcfd67b4ba8e7a60e9ebf3936cbc5fb4891b723
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-08 04:30:55 +0000

    create last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+if number >= 0:
+    last_digit = abs(number) % 10
+else:
+    last_digit = -(abs(number) % 10)
+
+output = f"Last digit of {number} is {last_digit}"
+if last_digit > 5:
+    output += " and is greater than 5"
+elif last_digit == 0:
+    output += " and is 0"
+else:
+    output += " and is less than 6 and not 0"
+
+print(output)
