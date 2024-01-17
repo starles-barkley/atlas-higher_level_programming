d4ff7f5dd5a510297c597a1ae3e501989cf4dddc
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 01:48:30 +0000

    write code from 3-square.py into 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class called square, with variables"""
+
+
+class Square:
+    """This is a class that defines a square"""
+    def __init__(self, size=0):
+        """This initializes a simple square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculates area of a square"""
+        return (self.__size**2)
+
