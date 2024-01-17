7414edb1125ab253aac2e8776b89173fac58634f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 05:10:10 +0000

    add a line after documentation on 0-add_integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a function that adds two integers"""
 
+
 def add_integer(a, b=98):
     """This function adds two integers together and returns the sum"""
     if not isinstance(a, (int, float)):
