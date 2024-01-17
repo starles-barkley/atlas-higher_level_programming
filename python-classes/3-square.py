81ccb65ce880572f777f40990d4df484b4451d9b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 01:45:46 +0000

    add def for area in 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,3 +12,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    def area(self):
+        """Calculates area of a square"""
+        return (self.__size**2)
