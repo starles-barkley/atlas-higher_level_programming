380ad510d315b78ef99be1ae695bbf5304219afe
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 05:22:17 +0000

    add new line after documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@
 
 def say_my_name(first_name, last_name+""):
     """This function prints a name"""
+    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
