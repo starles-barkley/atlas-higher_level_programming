89cf5eb571379bd90a38e8d84302f75b84e46009
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 02:11:03 +0000

    change the print def functionality

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -29,12 +29,12 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        """this prints the square with a special character to stdout"""
         if self.__size == 0:
             print("")
-        else:
-            for t in range(self.__size):
-                for x in range(self.__size):
-                    print("#", end="")
-                print("")
+            return
 
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
