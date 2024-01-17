9cea2ad79389d4a2163d1f189c5c80bf94fc1ad9
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 02:47:39 +0000

    add code from the square.py project

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,3 +17,31 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = height
 
+    @property
+    def width(self):
+        """This method defines its width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This method sets value to its width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """This method defines its height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This method sets value to its height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+        
