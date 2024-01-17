2f0d959e35019d5312259982db4bfde64865ccbf
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 05:28:17 +0000

    recreate 3-say_my_name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""This file defines a function which prints a name"""
+"""This file defines a function that prints a name"""
 
 
-def say_my_name(first_name, last_name+""):
+def say_my_name(first_name, last_name=""):
     """This function prints a name"""
-    
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
