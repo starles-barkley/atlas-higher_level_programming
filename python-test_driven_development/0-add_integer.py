4221398674c343f991e472a52cf5f1465d0b8a1a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 04:36:02 +0000

    draft code that adds two integers together and has typeerrors for non integer input

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a function that adds two integers"""
+
+def add_integer(a, b=98):
+    """This function adds two integers together and returns the sum"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a + b)
