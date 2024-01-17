c6ef3a0f38786324f504503e00967b652da9e3d4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-17 01:50:56 +0000

    add private attributes to 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,3 +16,14 @@ class Square:
         """Calculates area of a square"""
         return (self.__size**2)
 
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
