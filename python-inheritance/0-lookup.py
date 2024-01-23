95034ac4e082cda7f2e25b1d5e6196526e915918
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-23 23:58:55 +0000

    add another blank line bewtween the module documentation and the prototype on 0-lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''This is a module that outlines an object that returns a list'''
 
+
 def lookup(obj):
     '''This is a function that takes an object and returns a list'''
     return dir(obj)
