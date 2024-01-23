326fce8154875623c00b873d10080a212c495501
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-23 23:57:14 +0000

    change documentation from # to single quotes

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# This is a module that outlines an object and returns a list
+'''This is a module that outlines an object that returns a list'''
 
 def lookup(obj):
-    #This is a function that takes an object and returns a list
+    '''This is a function that takes an object and returns a list'''
     return dir(obj)
