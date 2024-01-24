fe59273a7009f8a20bdd0611ddafccfb0f6769b0
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 00:05:46 +0000

    correct name of 1-my_list and add documentation

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''This is a module that has a class that inherits a list'''
+
+class MyList(list):
+    '''This is a class that that inherits from a list'''
+
+    def print_sorted(self):
+        ''' This is a function that prints an inherited list in ascending order'''
+        sorted_list = sorted(self)
+        
--- python-inheritance/1-mylist.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-'''This is a module that has a class that inherits a list'''
-
-
-def print_sorted(self):
-    '''This is a class that that inherits from a list'''
-    
