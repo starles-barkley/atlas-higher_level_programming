8b5778b6b9923d5d713a2e02a8a528c055e09f6f
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 01:56:17 +0000

    create function __init__ with instantiation method in 8-rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+'''This module contains a class named Rectangle that is inherited'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    '''This is a class named rectangle that is inherited'''
+    def __init__(self, width, height):
+        """Instantiation method"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
