3798cd9d680b61b97f1758f0774e666daddba526
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 01:25:29 +0000

    add base code from previous task for 7-base_geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+'''This is a module that defines a class named BaseGeometry'''
+
+
+class BaseGeometry:
+    '''This defines a class named BaseGeometry'''
+    def area(self):
+        '''Public instance method'''
+        raise Exception("area() is not implemented")
