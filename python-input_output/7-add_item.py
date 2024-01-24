4f3b46a45fca079326a3bd659d9a5e081c4931a9
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 02:36:38 +0000

    create 7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines a function that adds all arguments to a Python list
+then saves them to a file
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import_module = __import__('6-load_from_json_file')
+load_from_json_file = import_module.load_from_json_file
+
+
