31c8542ea9b5f8fb76a9930ec6ea4adb0879ae75
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 02:22:05 +0000

    write code that reads a file and prints contents to standard output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/bash
+'''This is a module that reads a file'''
+
+
+def read_file(filename=""):
+    """This function reads from a file and prints to stdout"""
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
