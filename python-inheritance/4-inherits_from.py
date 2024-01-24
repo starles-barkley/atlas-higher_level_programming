ee63c552da4d6654ca890c67ecedb90173bb12c2
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 00:53:35 +0000

    write function and documentation for 4-inherits_from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+'''This module creates a function that identifies
+an object's subclass'''
+
+
+def inherits_from(obj, a_class):
+    '''This function identifies an object's subclass'''
+    return(issubclass(type(obj), a_class)and type(obj) is not a_class)
