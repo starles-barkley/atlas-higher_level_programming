dc801800fc506316caec3f2a80c29e775f524ba6
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 00:47:01 +0000

    remove whitespace

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,6 +4,6 @@ an object is an instance of a class or inherited'''
 
 
 def is_kind_of_class(obj, a_class):
-    '''This function determines if an object is 
+    '''This function determines if an object is
     and instance of a class or inherited '''
     return isinstance(obj, a_class)
