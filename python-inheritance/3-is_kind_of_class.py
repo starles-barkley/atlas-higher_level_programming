843a5dbac85fe08af87ce133e4ffed0de76c92eb
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 00:39:15 +0000

    create function and documentation in 3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/bash
+'''This module has a function that checks whether
+an object is an instance of a class or inherited'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''This function determines if an object is 
+    and instance of a class or inherited '''
+    return isinstance(obj, a_class)
