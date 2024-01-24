a9ff5cc7341ec35d690eda45027e5299200bfc1c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 00:23:07 +0000

    write the is_same class function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+''' This module outlines a function that returns whether two classes are the same'''
 
+
+def is_same_class(obj, a_class):
+    '''This function returns whether an object came from a specified class'''
+    return (type(obj)) is a_class
