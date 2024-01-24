fbea7a10d66d5b414d9557ed88ab12eef72b87a5
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-24 01:59:53 +0000

    add area and str to 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""This module defines a class Rectangle that is inherited"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This defines a class Rectangle that is inherited"""
+    def __init__(self, width, height):
+        """Instantiation method"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """This creates a function that returns the area of a rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """This function returns the string representation of Rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
