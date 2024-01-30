26cacae9ab4045ccba7344232a428d8426f100f6
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-30 18:37:51 +0000

    add else statement to instance of class with id

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,8 +6,11 @@ import json
 class Base:
     """This is a class named Base"""
     __nb_objects = 0
-    
+
       def __init__(self, id=None):
         """This method is the class constructor"""
         if id is not None:
             self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
