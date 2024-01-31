273fa0646115692df1e004b76547673e53f2b6b9
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 02:37:00 +0000

    correct indentation on display method in rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,7 +80,7 @@ class Rectangle(Base):
 
     def display(self):
         """Print the Rectangle instance with the character '#'"""
-       for _ in range(self.y):
+        for _ in range(self.y):
             print()
         for _ in range(self.height):
             print(' ' * self.x + '#' * self.width)
