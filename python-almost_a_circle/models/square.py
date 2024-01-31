616c24cfdd7a37318c2674e0e149bf22f7622c14
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 02:57:37 +0000

    add constructor for square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,3 +6,6 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """This is a class named Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """This is the class constructor for Square"""
+        super().__init__(size, size, x, y, id)
