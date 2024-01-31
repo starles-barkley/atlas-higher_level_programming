8ea5cec6151c8d903bc079fb7bd773e5f862dae7
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 02:59:34 +0000

    add attributes @property & @size.getter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,3 +9,19 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """This is the class constructor for Square"""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Getter for size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value <= 0:
+            raise ValueError("size must be > 0")
+        else:
+            self.width = value
+            self.height = value
