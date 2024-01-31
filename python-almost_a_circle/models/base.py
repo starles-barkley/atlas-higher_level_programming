f6b92ebd3c63e1454a6fef479ff6bdc9b62e2b3c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 03:29:08 +0000

    add if & else statments to the static method in the base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,3 +20,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
