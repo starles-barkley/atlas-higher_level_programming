6cf285e5da991d5b92922bcdefb33223b94e8ede
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 03:30:47 +0000

    write the classmethod save to file in the base class with documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -24,3 +24,9 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
