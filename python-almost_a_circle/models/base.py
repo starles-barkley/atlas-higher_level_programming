01fb89596a8d1670d2ceac90328cb52fcc0082f5
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 03:37:27 +0000

    remove extra whitespaces and add new line

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,7 +25,6 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
-
     @classmethod
     def save_to_file(cls, list_objs):
         """Write the JSON string representation of list_objs to a file"""
