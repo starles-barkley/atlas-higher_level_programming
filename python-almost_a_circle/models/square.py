15912593c70efd0723f336abe2dd1bd0ef72c319
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 02:55:56 +0000

    writet the square class, import rectangle, and write documentation

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a class named Square that inherits from Rectangle"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This is a class named Square that inherits from Rectangle"""
