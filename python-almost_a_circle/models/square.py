937526bca2f3f3c277191dcaf2676cc0bad0eb43
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 03:25:31 +0000

    remove whitespace from square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -35,7 +35,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-    
+
     def to_dictionary(self):
         """Return dictionary representation of the Square instance"""
         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
