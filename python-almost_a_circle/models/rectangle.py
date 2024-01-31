37a8d0d7e76c7d8479bd333017107a4490ab00da
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 01:33:50 +0000

    create instance of rectangle with width and base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,3 +7,9 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle class that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
