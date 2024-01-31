66d60025f7bebfbd53f697b807caa53fb290765a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-31 04:47:09 +0000

    convert imports to relative paths

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import unittest
-from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
+from ...models.base import Base
+from ...models.rectangle import Rectangle
+from ...models.square import Square
 from io import StringIO
 import sys
 
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import unittest
-from models.base import Base
-from models.square import Square
+from ...models.base import Base
+from ...models.square import Square
 from io import StringIO
 
 class TestSquareAttrs(unittest.TestCase):
