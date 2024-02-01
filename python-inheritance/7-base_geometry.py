0dbdf907823ede3b4d1e9482112feb5e840b3b77
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-01 05:52:17 +0000

    attempt to fix integer validator error in 7-base_geometry

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,9 +8,8 @@ class BaseGeometry:
         '''Public instance method'''
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name, value):
-        """public instance method that validates value"""
-        if type(value) != int:
+     def integer_validator(self, name, value):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
