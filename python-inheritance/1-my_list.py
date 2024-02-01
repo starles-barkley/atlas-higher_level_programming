5615a2ff1ddb8f08708820dac9c6b94c37f220a0
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-01 05:43:08 +0000

    fix errors in 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,8 +7,6 @@ class MyList(list):
     '''This is a class that that inherits from a list'''
 
     def print_sorted(self):
-        ''' This is a function that prints an inherited list in ascending order'''
+        ''' This is a function that prints list in ascending order'''
         sorted_list = sorted(self)
         print(sorted_list)
-
-        
