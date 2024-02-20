63dac20289773484c2c8c419734bb302c6c943e8
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 16:22:03 -0600

    add  where to 12-no_genre.sql

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
 -- Selects shows from database with no associated genre
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id 
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
