db28ee0ed73fa0175e9011246eb224cdedc15c41
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 16:18:16 -0600

    add ordering by title 11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- Displays all shows in a database
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
