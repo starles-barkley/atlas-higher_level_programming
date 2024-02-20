dd45f32d60a59556a72215fb8a7c47a6efa6c664
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 16:25:49 -0600

    add inner join to count shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,3 @@
 -- Counts shows in database by genre
 SELECT DISTINCT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
