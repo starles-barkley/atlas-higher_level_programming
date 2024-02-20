0dab80444500c2bcd96ba10b5dc910ed00d57653
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 15:09:04 -0600

    add semicolon to database in 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Create database and table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
