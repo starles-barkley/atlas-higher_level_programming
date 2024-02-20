9b5d399822cd324f518ef4da2611b18672ca2051
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 15:13:54 -0600

    add cities table to usa database 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,7 @@
 -- Creates a database and a table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+)
