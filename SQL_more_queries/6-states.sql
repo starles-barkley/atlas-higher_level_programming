d580e53d0b3b2a647bd3bd457388b23f6841113b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 15:06:53 -0600

    Change id to be INT and auto increment 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
 CREATE TABLE IF NOT EXISTS hbtn_0d_states (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256)
+    id INT NOT NULL AUTO_INCREMENT UNIQUE
+    name VARCHAR(256) NOT NULL
 )
