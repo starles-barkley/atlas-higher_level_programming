252653cdb4fdcec07a2a6d95baeeb74230e2eacf
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 15:10:58 -0600

    add usa to beginning of table name 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
