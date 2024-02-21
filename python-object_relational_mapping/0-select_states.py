20071b911a693d550e7fb361b5f500b27721b854
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 20:37:56 -0600

    i don't think this will work

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,12 +4,29 @@
 import MySQLdb
 import sys
 
-def sql_connect(username, password, db_name):
+def sql_connect(usr, pw, db_name):
     """This function imports a database to use"""
-    try:
-        """ Connect to the MySQL server"""
-        connection = MySQLdb.connect(host="localhost",
-                                     port=3306,
-                                     user=username,
-                                     passwd=password,
-                                     db=database_name)
+
+    """Establish connection"""
+    db = MySQLdb.connect(host="localhost",
+                         user=usr,
+                         port=3306,
+                         passwd=pw,
+                         database=db_name)
+
+    """Create cursor object"""
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+
+    """Access the queried data to print"""
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    """Close the connection and cursor object"""
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
