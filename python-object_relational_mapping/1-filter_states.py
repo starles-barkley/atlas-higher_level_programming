b55b4982f7f712d87d8088a9dbba54d12306a842
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-21 03:18:00 +0000

    ad similar contents as previous file to test

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Script that lists all states in a database """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', user=user, passwd=pwd, db=database)
+    cur = db.cursor()
+
+    staterows = cur.execute("SELECT * FROM states ORDER BY states.id;")
+    for row in cur._rows:
+        print(row)
