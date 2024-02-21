aa500b556556fa515192284be3fd3e3e71de46f4
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-21 03:14:49 +0000

    add columns

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,18 @@ import sys
 
 if __name__ == '__main__':
 
+    import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+
     user = sys.argv[1]
     pwd = sys.argv[2]
     database = sys.argv[3]
-	@@ -15,4 +16,3 @@
+    db = MySQLdb.connect(host='localhost', user=user, passwd=pwd, db=database)
+    cur = db.cursor()
+
+    staterows = cur.execute("SELECT * FROM states ORDER BY states.id;")
     for row in cur._rows:
         print(row)
