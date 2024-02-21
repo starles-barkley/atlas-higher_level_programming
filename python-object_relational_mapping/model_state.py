6d391deb7ccaebb471c76b5f90b556a0e0e08b14
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-21 04:06:34 +0000

    create module with state class that inherits from base class

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Module with State SQLAlchemy definition """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", back_populates="state")
