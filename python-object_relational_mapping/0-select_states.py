72d4ffa0ca2406d55b4da51bc233ee7a88056767
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 20:50:04 -0600

    try different approach without function

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a function that establishes a connection to a SQL server"""
+
+""" Script that lists all states in a database """
 
 import MySQLdb
 import sys
 
-def sql_connect(usr, pw, db_name):
-    """This function imports a database to use"""
 
-    """Establish connection"""
-    db = MySQLdb.connect(host="localhost",
-                         user=usr,
-                         port=3306,
-                         passwd=pw,
-                         database=db_name)
-
-    """Create cursor object"""
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host='localhost', user=user, passwd=pwd, db=database)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states;")
 
-    """Access the queried data to print"""
-    rows = cur.fetchall()
-    for row in rows:
+    staterows = cur.execute("SELECT * FROM states ORDER BY states.id;")
+    for row in cur._rows:
         print(row)
-
-    """Close the connection and cursor object"""
-    cur.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    sql_connect(sys.argv[1], sys.argv[2], sys.argv[3])
+        
+if __name__ == '__main__':
