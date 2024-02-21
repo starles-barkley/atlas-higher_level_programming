afc82561bcfc44d678b2dab37fde299ff6539fae
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 20:34:06 -0600

    add try to sql_connect to attempt connecting to server

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,5 +4,12 @@
 import MySQLdb
 import sys
 
-def sql_connect(usr, pw, db_name):
+def sql_connect(username, password, db_name):
     """This function imports a database to use"""
+    try:
+        """ Connect to the MySQL server"""
+        connection = MySQLdb.connect(host="localhost",
+                                     port=3306,
+                                     user=username,
+                                     passwd=password,
+                                     db=database_name)
