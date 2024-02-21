fe2cbe8607feb7dc0813caa814f7d4dfa011d957
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-02-20 21:01:39 -0600

    revert back to previous version (i just need to make it executable'

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,47 +1,13 @@
 #!/usr/bin/python3
-""" This script lists all states in a database """ 
-import sys
+""" Script that lists all states in a database """
 import MySQLdb
+import sys
 
-def list_states(username, password, database_name):
-    try:
-        # Connect to the MySQL server
-        connection = MySQLdb.connect(host="localhost",
-                                     port=3306,
-                                     user=username,
-                                     passwd=password,
-                                     db=database_name)
+if __name__ == '__main__':
 
-        # Create a cursor object to execute SQL queries
-        cursor = connection.cursor()
-
-        # Execute the SQL query to retrieve states
-        query = "SELECT * FROM states ORDER BY states.id ASC"
-        cursor.execute(query)
-
-        # Fetch all the rows
-        rows = cursor.fetchall()
-
-        # Display the results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
-    finally:
-        # Close the cursor and connection
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-
-if __name__ == "__main__":
-    # Check if all 3 arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
-    else:
-        # Extract arguments
-        mysql_username, mysql_password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-        # Call the function to list states
-        list_states(mysql_username, mysql_password, database_name)
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+	@@ -15,4 +16,3 @@
+    for row in cur._rows:
+        print(row)
