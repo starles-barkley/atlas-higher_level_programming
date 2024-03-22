71c7e451dc0a822c018f9a890c931148a63c1095
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-22 21:16:55 +0000

    change let to const

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let numArgs = process.argv.length - 2;
+const numArgs = process.argv.length - 2;
 
 if (numArgs === 0) {
     console.log('No argument');
@@ -8,4 +8,3 @@ if (numArgs === 0) {
   } else {
     console.log('Arguments found');
   }
-  
