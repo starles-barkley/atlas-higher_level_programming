1b5e010f5bb4e4fe1961cad3f11ebdd9a671951c
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 17:48:10 +0000

    fix syntax erro

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -8,7 +8,7 @@ if (isNaN(parseInt(size))) {
   const squareSize = parseInt(size);
   for (let i = 0; i < squareSize; i++) {
       let row = "";
-      for (let j = 0; j < squareSize; j++) 
+      for (let j = 0; j < squareSize; j++) {
           row += "X";
       }
       console.log(row);
