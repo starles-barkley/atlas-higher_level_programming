2d0714bf4c00b44a1621aa44e8542ff13db75082
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 18:24:27 +0000

    create script that finds the 2nd biggest number

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(arg => parseInt(arg));
+
+if (args.length === 0 || args.length === 1) {
+    console.log(0);
+} else {
+    let max = -Infinity;
+    let secondMax = -Infinity;
+
+    console.log(secondMax);
+}
