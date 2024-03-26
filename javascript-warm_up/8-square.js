40b8eaa01803ec5073f33b2821c6ff6b82df3300
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 17:47:35 +0000

    add for loop

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -8,4 +8,9 @@ if (isNaN(parseInt(size))) {
   const squareSize = parseInt(size);
   for (let i = 0; i < squareSize; i++) {
       let row = "";
-      
+      for (let j = 0; j < squareSize; j++) 
+          row += "X";
+      }
+      console.log(row);
+  }
+}
