7294ad268d62f58da56b0d4f7ddd3e46458314f8
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 18:31:23 +0000

    adhere to semistandard style

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,19 +3,19 @@
 const args = process.argv.slice(2).map(arg => parseInt(arg));
 
 if (args.length === 0 || args.length === 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    let max = -Infinity;
-    let secondMax = -Infinity;
+  let max = -Infinity;
+  let secondMax = -Infinity;
 
-    for (let i = 0; i < args.length; i++) {
-        if (args[i] > max) {
-            secondMax = max;
-            max = args[i];
-        } else if (args[i] > secondMax && args[i] !== max) {
-            secondMax = args[i];
-        }
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > max) {
+      secondMax = max;
+      max = args[i];
+    } else if (args[i] > secondMax && args[i] !== max) {
+      secondMax = args[i];
     }
+  }
 
-    console.log(secondMax);
+  console.log(secondMax);
 }
