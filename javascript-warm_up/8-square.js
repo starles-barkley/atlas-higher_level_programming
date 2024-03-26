dcb35ab99ae2b09b72eb194bc723db106dd839ab
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 17:51:40 +0000

    fix semistandard validation

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,14 +3,14 @@
 const size = process.argv[2];
 
 if (isNaN(parseInt(size))) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   const squareSize = parseInt(size);
   for (let i = 0; i < squareSize; i++) {
-      let row = "";
-      for (let j = 0; j < squareSize; j++) {
-          row += "X";
-      }
-      console.log(row);
-  }
+    let row = "";
+    for (let j = 0; j < squareSize; j++) {
+        row += "X";
+    }
+    console.log(row);
+}
 }
