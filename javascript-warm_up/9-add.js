c906c3166c17bbcfd2dec8191302e7883e311deb
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 18:14:41 +0000

    add if and else statements to handle printing

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -7,3 +7,8 @@ function add(a, b) {
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
+if (isNaN(parseInt(arg1)) || isNaN(parseInt(arg2))) {
+  console.log('Invalid input. Please provide two integers.');
+} else {
+  console.log('The sum is:', add(arg1, arg2));
+}
