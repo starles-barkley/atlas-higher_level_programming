f2f755aa85446467afafaf11ad4dbf952001266b
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 18:12:54 +0000

    create script that computes factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function computeFactorial(num) {
+  if (isNaN(parseInt(num))) {
+      return 1;
+  } else if (parseInt(num) <= 1) {
+      return 1;
+  } else {
+      return parseInt(num) * computeFactorial(parseInt(num) - 1);
+  }
+}
+
+const input = process.argv[2];
+console.log('Factorial of', input, 'is:', computeFactorial(input));
