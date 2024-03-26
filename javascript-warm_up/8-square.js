cbb23133a780d3d5d105e92b521b6e4694baefcc
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-03-26 17:40:29 +0000

    add else statement to create sqaure shape

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -5,3 +5,7 @@ const size = process.argv[2];
 if (isNaN(parseInt(size))) {
     console.log("Missing size");
 } else {
+  const squareSize = parseInt(size);
+  for (let i = 0; i < squareSize; i++) {
+      let row = "";
+      
