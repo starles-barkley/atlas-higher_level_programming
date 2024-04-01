e5607cd61a6b5bdc4859ed788c0a3af9158278fe
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-04-01 22:56:23 +0000

    add method to read file path

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -6,3 +6,13 @@ if (process.argv.length < 3) {
   console.log('Usage: node read_file.js <file_path>');
   process.exit(1);
 }
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+})
