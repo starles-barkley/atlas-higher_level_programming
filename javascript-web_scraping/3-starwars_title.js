ad58e2883e21a7d9251883cddfcc61dff6533b30
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-04-02 02:09:54 +0000

    add else statement

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -5,4 +5,8 @@ const { argv } = require('node:process');
 request(`https://swapi-api.hbtn.io/api/films/${argv[2]}`, (err, res, body) => {
   if (err) {
     console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
