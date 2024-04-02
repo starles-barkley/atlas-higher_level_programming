b4ce4d57ed5f0265340e6bd4f60d5c63b2d5d358
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-04-02 00:00:30 +0000

    scratch previous code and simplify the script

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const { argv } = require('node:process');
 
-if (process.argv.length < 4) {
-  console.log('Usage: node write_file.js <file_path> <content>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(argv[2], argv[3], err => {
   if (err) {
-    console.error(err);
-  } else {
-    console.log('Content has been written to the file successfully.');
+    throw err;
   }
 });
